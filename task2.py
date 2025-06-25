2efc659f7fa31ff2060960d5aaa3a1b1673388f4
husnamuhammadali <husnamuhammadali1@gmail.com>
2025-06-25 10:15:44 +0500

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,4 @@
+print("Name: Husna")
+print("Hobby:Reading")
+print("Age:19")
+print("I want to learn Python beacause its use in web development , data science")
---
+++ task3.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter 2nd number"))
+result = num1 + num2
+print("the sum is:",result)
